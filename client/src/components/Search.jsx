15058942a3fd8c753e57c8384d046acdad454e40
Will <wkennedy210@gmail.com>
2020-12-03 11:08:21 -0500

    add functionality to search for users

---
+++ client/src/components/Search.jsx
@@ -0,0 +1,68 @@
+import React, { useState, useEffect, useRef } from 'react';
+import { Form } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import { Avatar } from '@material-ui/core';
+import axios from 'axios';
+
+const Search = () => {
+  const [display, setDisplay] = useState(false);
+  const [options, setOptions] = useState([]);
+  const [search, setSearch] = useState('');
+  const wrapperRef = useRef(null);
+
+  const getUsers = async () => {
+    try {
+      const { data } = await axios.get('/api/users', { withCredentials: true });
+      setOptions(data);
+    } catch (error) {
+      alert(error.message);
+    }
+  };
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  useEffect(() => {
+    document.addEventListener('mousedown', handleClickOutside);
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
+
+  const handleClickOutside = (event) => {
+    const { current: wrap } = wrapperRef;
+    if (wrap && !wrap.contains(event.target)) {
+      setDisplay(false);
+    }
+  };
+
+  const handleChange = (e) => {
+    setSearch(e.target.value);
+    setDisplay(true);
+  };
+
+  return (
+    <div ref={wrapperRef}>
+      <Form.Control type="text" name="search" onChange={handleChange} />
+      {display && (
+        <div>
+          {options
+            .filter(
+              ({ username }) => username.indexOf(search.toLowerCase()) > -1
+            )
+            .map((user) => (
+              <Link key={user._id} to={`/users/${user._id}`}>
+                <div>
+                  <Avatar src={user.avatar} alt={user.username} />
+                  <p>{user.username}</p>
+                </div>
+              </Link>
+            ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Search;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
 import Feed from '../components/Feed';
+import Search from '../components/Search';
 import LogoutButton from '../components/LogoutButton';
 
 const Home = () => {
   return (
     <Container>
       <LogoutButton />
+      <Search />
       <Feed />
     </Container>
   );
