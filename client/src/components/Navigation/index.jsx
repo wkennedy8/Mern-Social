8a0af8c64f7337ed0a3953840d7cd78c6a6b401a
Will <wkennedy210@gmail.com>
2021-01-27 23:31:13 -0500

    initial navbar

--- client/src/components/Navigation/index.jsx
+++ client/src/components/Navigation/index.jsx
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { AppContext } from '../../context/AppContext';
+import { RiArrowDownSFill } from 'react-icons/ri';
 import styles from './navigation.module.css';
 
 const Navigation = () => {
+  const { currentUser } = useContext(AppContext);
   return (
     <div className={styles.container}>
-      <h1>Navigation</h1>
+      <nav className={styles.nav}>
+        <h1 className={styles.heading}>Fakebook</h1>
+        {currentUser && (
+          <div className={styles.user}>
+            <img
+              className={styles.profileImage}
+              src="https://files.willkennedy.dev/social_media_app/default-image.png"
+              alt={`${currentUser.username} profilePic`}
+            />
+            <p>{currentUser.username}</p>
+            <RiArrowDownSFill className={styles.dropdownArrow} />
+          </div>
+        )}
+      </nav>
     </div>
   );
 };
